008f4c768856a0d91c9caa020dba24bf5a0f42c5
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2022-10-23 21:39:10 +0700

    added command add

--- app.ts
+++ app.ts
@@ -1,1 +1,34 @@
-console.log(process.argv);
+const yargs = require("yargs");
+
+yargs.command({
+  command: "add",
+  describe: "Menambahkan kontak baru",
+  builder: {
+    nama: {
+      describe: "Nama lengkap",
+      demandOption: true,
+      type: "string",
+    },
+    email: {
+      describe: "Email",
+      demandOption: false,
+      type: "string",
+    },
+    noHP: {
+      describe: "Nomor Handphone",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    const contact = {
+      nama: argv.nama,
+      email: argv.email,
+      noHP: argv.noHP,
+    };
+
+    console.log(contact);
+  },
+});
+
+yargs.parse();
